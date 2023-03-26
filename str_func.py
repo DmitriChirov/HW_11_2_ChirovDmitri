8b704f15dc94a4f745e1c6359341366a18581fca
Dmitri Chirov <dim.ch.it80@gmail.com>
2023-03-26 23:42:55 +0300

    add function. add docstrings in all functions

--- str_func.py
+++ str_func.py
@@ -1,2 +1,8 @@
 def str_upper(string_value):
+    '''get a string and return it with all capital letters'''
     return string_value.upper()
+
+
+def str_title(string_value):
+    '''get a string and return it with first capital letters in each word'''
+    return string_value.title()
